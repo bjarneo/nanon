8ff1c59a1f4ff24e7ade3678c1fb0ec33601f4a9
bjarneo <bjarne.overli@vg.no>
2017-01-29 15:08:29 +0100

    use webpack merge

--- src/webpack-config.js
+++ src/webpack-config.js
@@ -1,4 +1,4 @@
-const merge = require('lodash.merge');
+const merge = require('webpack-merge');
 const ClosureCompiler = require('google-closure-compiler-js').webpack;
 
 module.exports = function getWebpackConfig(opts) {
